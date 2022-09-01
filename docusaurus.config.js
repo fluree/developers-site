732da1756c0521c98f8ad9c2c14983beb99d096f
Flor Marshall <flordmarshall@gmail.com>
2022-09-01 10:23:37 -0400

    add new logos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,7 +40,7 @@ module.exports = {
       // Main logo which links to fluree marketing site
       logo: {
         alt: "Fluree Yeti Logo",
-        src: "img/Dark-Horizontal.svg",
+        src: "img/fluree-logo@3x.png",
         srcDark: "img/White-Horizontal.svg",
         href: "https://flur.ee",
       },
@@ -99,9 +99,9 @@ module.exports = {
 
         // Navbar links - right aligned
         {
-          href: 'https://flur.ee/try',
-          label: 'Nexus Beta Sign-Up',
-          position: 'right',
+          href: "https://flur.ee/try",
+          label: "Nexus Beta Sign-Up",
+          position: "right",
         },
         {
           href: "https://github.com/fluree/developers-site",
