15e426bb0802421640c760d1b0697564fd59dbc0
Jonathan Dorety <jdorety@flur.ee>
2022-09-21 13:18:02 -0400

    Add ArrowTopRightOnBoxIcon component

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,12 +1,13 @@
+import type { SVGProps } from "react";
+
 export * from "./ArrowPathIcon";
 export * from "./ArrowsUpDownIcon";
 export * from "./ClockIcon";
 export * from "./CodeIcon";
 export * from "./SettingsIcon";
 export * from "./TableIcon";
+export * from "./ArrowTopRightOnBoxIcon";
 
-export type IconProps = {
-  height?: number;
-  width?: number;
+export type IconProps = Pick<SVGProps<SVGSVGElement>, "height" | "width"> & {
   color?: string;
-}
+};
