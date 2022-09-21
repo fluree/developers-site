00897637afcb4df557d2646525c1fa03163ee8f6
Jonathan Dorety <jdorety@flur.ee>
2022-09-21 09:41:44 -0400

    Add icon components

--- src/components/Icons/CodeIcon.jsx
+++ src/components/Icons/CodeIcon.jsx
@@ -8,13 +8,13 @@ export const CodeIcon = () => {
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
-      stroke-width="2"
+      strokeWidth="2"
       stroke="#00A0D1"
       style={{position: "relative", bottom: 8}}
     >
       <path
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeLinecap="round"
+        strokeLinejoin="round"
         d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5"
       />
     </svg>
--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -1,2 +1,6 @@
 export * from "./CodeIcon";
 export * from "./TableIcon";
+export * from "./ArrowsUpDownIcon";
+export * from "./ClockIcon";
+export * from "./SettingsIcon";
+export * from "./ArrowPath";
