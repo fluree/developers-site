2d121cc2494156a9f5ba57b19899fb5b2ad872f1
Jonathan Dorety <jdorety@flur.ee>
2023-10-04 13:08:57 -0400

    Update Reo script

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -246,10 +246,10 @@ module.exports = {
     {
       src: "https://static.reo.dev/cb8a71410d7f8f8/reo.js",
       async: true,
-      onload: function () {
-        Reo.init({
-          clientID: "cb8a71410d7f8f8",
-        });
+      onLoad: () => {
+        if (typeof window !== "undefined") {
+          window.Reo && window.Reo.init({ clientID: "cb8a71410d7f8f8" });
+        }
       },
     },
   ],
